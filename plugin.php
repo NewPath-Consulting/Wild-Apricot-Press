7c585eab6f910ab1a349069360b0048a85f770f1
Spencer Gable-Cook <sgablec@umich.edu>
2021-05-19 19:50:12 -0400

    WA4WP menu added

--- plugin.php
+++ plugin.php
@@ -1,7 +1,8 @@
 <?php
-namespace WAWP;
+
+// namespace WAWP;
 /**
- * Plugin Name:       Wild Apricot for WordPress
+ * Plugin Name:       Wild Apricot for WordPress (WA4WP)
  * Plugin URI:        https://newpathconsulting.com/wild-apricot-for-wordpress
  * Description:       Integrates your Wild Apricot account with your WordPress website!
  * Version:           1.0.0
@@ -33,13 +34,56 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
+do_action('qm/debug', 'outside!');
+
 // Activation hook
 register_activation_hook(__FILE__, function() {
+	// do_action('qm/debug', 'activation in plugin.php!');
+	add_option('hello_table', 'hello_table');
 	require_once plugin_dir_path(__FILE__) . 'src/Activator.php';
 	Activator::activate();
 } );
 
 // Add menu
+add_action( 'admin_menu', 'wawp_create_menu' );
+
+function wawp_create_menu() {
+
+    //create custom top-level menu
+    add_menu_page( 'WA4WP Settings Page', 'WA4WP',
+        'manage_options', 'wawp-options', 'wawp_settings_page',
+        'dashicons-businesswoman', 6 );
+
+    //create submenu items
+    // add_submenu_page( 'pdev-options', 'About The PDEV Plugin', 'About', 'manage_options',
+    //     'pdev-about', 'pdev_about_page' );
+    // add_submenu_page( 'pdev-options', 'Help With The PDEV Plugin', 'Help', 'manage_options',
+    //     'pdev-help', 'pdev_help_page' );
+    // add_submenu_page( 'pdev-options', 'Uninstall The PDEV Plugin', 'Uninstall', 'manage_options',
+    //     'pdev-uninstall', 'pdev_uninstall_page' );
+
+}
+
+//placerholder function for the settings page
+function wawp_settings_page() {
+	// Display WA4WP Settings page
+
+}
+
+// //placerholder function for the about page
+// function pdev_about_page() {
+
+// }
+
+// //placerholder function for the help page
+// function pdev_help_page() {
+
+// }
+
+// //placerholder function for the uninstall page
+// function pdev_uninstall_page() {
+
+// }
 
 // Deactivation hook
 register_deactivation_hook(__FILE__, function() {
--- src/Activator.php
+++ src/Activator.php
@@ -1,16 +1,24 @@
 <?php
 
-namespace WAWP;
+// namespace WAWP;
 
 class Activator {
-  /**
-   * Activates the WA4WP plugin.
-   *
-   * Write the full details of what happens here.
-   */
-  public static function activate() {
-    // Activation code
-    // Set up menu
-  }
+	function wawp_create_menu() {
+		add_option('wawp_create_menu_key', 'wawp_create_menu_yo');
+		add_menu_page('WA4WP Settings Page', 'WA4WP', 'manage_options', 'wawp-options', 'wawp_settings_page', 'dashicons-businesswoman', 99);
+	}
+
+	/**
+	 * Activates the WA4WP plugin.
+	 *
+	 * Write the full details of what happens here.
+	 */
+	public static function activate() {
+		// Activation code
+		// Set up menu
+		add_option('we_activate', 'we_activate');
+		// wawp_create_menu();
+		// add_action('admin_menu', 'wawp_create_menu');
+	}
 }
 ?>
