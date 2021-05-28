6098cd3713fb768ee52f18002e70da0fccba954b
Spencer Gable-Cook <sgablec@umich.edu>
2021-05-28 15:47:00 -0400

    Implemented encryption and decryption

--- src/DataEncryption.php
+++ src/DataEncryption.php
@@ -17,15 +17,48 @@ class DataEncryption {
 			return $value;
 		}
 
-		// Load encryption parameters
-		$method = 'aes-256-ctr';
-		$ivlen = openssl_cipher_iv_length($method);
-		$iv = openssl_random_pseudo_bytes($ivlen);
+		// Define and get encryption parameters
+		$encryption_method = 'aes-256-ctr';
+		$cipher_iv_len = openssl_cipher_iv_length($method);
+		$random_string = openssl_random_pseudo_bytes($ivlen);
+
+		// Encrypt raw value
+		$raw_value = openssl_encrypt($value . $this->salt, $encryption_method, $this->key, 0, $random_string);
+		// If encrypting the raw value was a failure, return false
+		if (!$raw_value) {
+			return false;
+		}
+
+		// Return encoded data
+		return base64_encode($andom_string . $raw_value);
 	}
 
 	// Decrypts value
-	public function decrypt( $value ) {
+	public function decrypt( $raw_value ) {
+		// Check that openssl library exists
+		if (!extension_loaded('openssl')) {
+			return $raw_value;
+		}
 
+		// Decode raw value
+		$raw_value = base64_decode($raw_value, true);
+
+		// Define and get encryption parameters
+		$encryption_method = 'aes-256-ctr';
+		$cipher_iv_len = openssl_cipher_iv_length($method);
+		$random_string = substr($raw_value, 0, $cipher_iv_len);
+
+		// Update raw value
+		$raw_value = substr($raw_value, $cipher_iv_len);
+
+		// Decrypt raw value to value
+		$value = openssl_encrypt($raw_value, $encryption_method, $this->key, 0, $random_string);
+		if (!$value || substr($value, -strlen($this->salt)) !== $this->salt) { // failed
+			return false;
+		}
+
+		// Return back value
+		return substr($value, 0, -strlen($this->salt));
 	}
 
 	private function get_default_key() {
