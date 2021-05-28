b0c8b485d6342dfcf9a3cf821ed24616363b2d95
Spencer Gable-Cook <sgablec@umich.edu>
2021-05-27 20:35:13 -0400

    Removes options upon deactivation

--- src/Deactivator.php
+++ src/Deactivator.php
@@ -3,9 +3,10 @@
 // namespace WAWP;
 
 class Deactivator {
-  public static function deactivate() {
-    // Delete entries in wp_options table
-
-  }
+	public static function deactivate() {
+		// Delete entries in wp_options table
+		require_once('Plugin.php');
+		delete_option(Plugin::WILD_APRICOT_KEY);
+	}
 }
 ?>
