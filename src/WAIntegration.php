067fedb1dd7158af7a0f34b39c678b93a76a837a
Spencer Gable-Cook <sgablec@umich.edu>
2021-06-04 11:12:05 -0400

    Decrypting WA credentials

--- src/WAIntegration.php
+++ src/WAIntegration.php
@@ -6,12 +6,18 @@ class WAIntegration {
 
 	public function __construct() {
 		// Hook that runs after Wild Apricot credentials are saved
-		add_action( 'wawp_wal_credentials_obtained', array( $this, 'get_user_credentials') );
+		add_action( 'wawp_wal_credentials_obtained', array( $this, 'load_user_credentials') );
 	}
 
-	public function get_user_credentials() {
+	public function load_user_credentials() {
 		// Load encrypted credentials from database
-		$this->credentials =
+		$this->credentials = get_option( 'wawp_wal_name' );
+		// Decrypt credentials
+
+		// Perform API request
+		$body = array(
+			'grant_type' => 'grant_type=client_credentials&scope=auto&obtain_refresh_token=true'
+		);
 	}
 }
 ?>
