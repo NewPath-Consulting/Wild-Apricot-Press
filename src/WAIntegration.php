46d22d7596eead4da06acfba60984b7361c3b09c
Spencer Gable-Cook <sgablec@umich.edu>
2021-06-08 17:32:29 -0400

    Fixing menu with login and logout

--- src/WAIntegration.php
+++ src/WAIntegration.php
@@ -3,6 +3,9 @@ namespace WAWP;
 
 class WAIntegration {
 	private $credentials;
+	private $access_token;
+	private $refresh_token;
+	private $base_wa_url;
 
 	public function __construct() {
 		// Hook that runs after Wild Apricot credentials are saved
@@ -13,7 +16,7 @@ class WAIntegration {
 		require_once('DataEncryption.php');
 	}
 
-	private function load_user_credentials() {
+	public function load_user_credentials() {
 		// Load encrypted credentials from database
 		$this->credentials = get_option('wawp_wal_name');
 		// print_r($this->credentials);
@@ -28,37 +31,74 @@ class WAIntegration {
 		$decrypted_credentials['wawp_wal_client_secret'] = $dataEncryption->decrypt($this->credentials['wawp_wal_client_secret']);
 		// Echo values for testing
 		// print_r($decrypted_credentials);
-		do_action('qm/debug', 'decrypt api key: ' . $decrypted_credentials['wawp_wal_api_key']);
-		do_action('qm/debug', 'decrypt client id: ' . $decrypted_credentials['wawp_wal_client_id']);
-		do_action('qm/debug', 'decrypt client secret: ' . $decrypted_credentials['wawp_wal_client_secret']);
+		// do_action('qm/debug', 'decrypt api key: ' . $decrypted_credentials['wawp_wal_api_key']);
+		// do_action('qm/debug', 'decrypt client id: ' . $decrypted_credentials['wawp_wal_client_id']);
+		// do_action('qm/debug', 'decrypt client secret: ' . $decrypted_credentials['wawp_wal_client_secret']);
 		// Encode API key
 		$api_string = 'APIKEY:' . $decrypted_credentials['wawp_wal_api_key'];
 		$encoded_api_string = base64_encode($api_string);
 		// Perform API request
-		$api_args = array(
+		$args = array(
 			'headers' => array(
 				'Authorization' => 'Basic ' . $encoded_api_string,
 				'Content-type' => 'application/x-www-form-urlencoded'
 			),
 			'body' => 'grant_type=client_credentials&scope=auto&obtain_refresh_token=true'
 		);
-		$api_response = wp_remote_post('https://oauth.wildapricot.org/auth/token', $api_args);
-		do_action('qm/debug', $api_response);
-
+		$response = wp_remote_post('https://oauth.wildapricot.org/auth/token', $args);
+		do_action('qm/debug', $response);
+		// Check that api response is valid -> return false if it is invalid
+		if (is_wp_error($response)) {
+			return false;
+		}
+		// Response is valid -> get body from response
+		$body = wp_remote_retrieve_body($response);
+		// Decode JSON string to array with 'true' parameter
+		$data = json_decode($body, true);
+		do_action('qm/debug', $data);
+		$this->access_token = $data['access_token'];
+		$this->refresh_token = $data['refresh_token'];
 		// Add navigation menu
+	}
+
+	public function get_base_api() {
+		$api_args = array(
+			'headers' => array(
+				'Authorization' => 'Bearer ' . $this->access_token,
+				'Accept' => 'application/json',
+				'User-Agent' => 'WildApricotForWordPress'
+			),
+		);
+		$api_response = wp_remote_post('https://api.wildapricot.org/', $api_args);
+		do_action('qm/debug', 'new api response!!!!' . $api_response);
+		return $api_response;
 	}
 
 	// see: https://developer.wordpress.org/reference/functions/wp_create_nav_menu/
 	// Also: https://www.wpbeginner.com/wp-themes/how-to-add-custom-items-to-specific-wordpress-menus/
-	private function create_wa_login_logout($items, $args) {
+	// https://wordpress.stackexchange.com/questions/86868/remove-a-menu-item-in-menu
+	public function create_wa_login_logout($items, $args) {
 		do_action('qm/debug', 'Adding login in menu!');
+		// Get login url based on user's Wild Apricot site
+		$login_url = $this->get_base_api();
+		$logout_url = '';
 		// Check if user is logged in or logged out
-		if (is_user_logged_in() && $args->theme_location == 'primary') {
+		if (is_user_logged_in() && $args->theme_location == 'primary') { // Logout
 			$items .= '<li><a href="'. wp_logout_url() .'">Log Out</a></li>';
-		} elseif (!is_user_logged_in() && $args->theme_location == 'primary') {
-			$items .= '<li><a href="'. site_url('wp-login.php') .'">Log In</a></li>';
+		} elseif (!is_user_logged_in() && $args->theme_location == 'primary') { // Login
+			$items .= '<li><a href="'. $login_url .'">Log In</a></li>';
 		}
 		return $items;
+	}
+
+	// Login actions
+	public function login_user_to_wa() {
+
+	}
+
+	// Logout actions
+	public function logout_user_to_wa() {
+
 	}
 }
 ?>
