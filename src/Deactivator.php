cf8e8b0d036f651513054402a361e600d0b78f75
Spencer Gable-Cook <sgablec@umich.edu>
2021-06-09 11:49:20 -0400

    Moved removing menu to deactivation instead of uninstall

--- src/Deactivator.php
+++ src/Deactivator.php
@@ -5,6 +5,12 @@ namespace WAWP;
 class Deactivator {
 	public static function deactivate() {
 		// Deactivation code
+
+		// Remove Login/Logout from the navigation bar
+		// First, get menu items of navigation menu
+		$menu_name = 'primary'; // will change this based on what user selects
+		$menu_items = wp_get_nav_menu_items($menu_name);
+		do_action('qm/debug', $menu_items);
 	}
 }
 ?>
--- uninstall.php
+++ uninstall.php
@@ -10,8 +10,4 @@ delete_option('wawp_wal_name');
 delete_option('wawp_addons');
 delete_option('wawp_license_keys');
 
-// Remove Login/Logout from the navigation bar
-// First, get menu items of navigation menu
-$menu_name = 'primary'; // will change this based on what user selects
-$menu_items = wp_get_nav_menu_items($menu_name);
 ?>
