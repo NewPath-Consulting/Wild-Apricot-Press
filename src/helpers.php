5dc495ec89ceee95e169d34317e5ebe71b54f04b
Natalie Brotherton <brona@umich.edu>
2022-06-14 14:55:53 -0400

    get rid of error log statements

--- src/helpers.php
+++ src/helpers.php
@@ -7,13 +7,11 @@ const CORE_NAME = 'NewPath Wild Apricot Press (WAP)';
 
 function is_licensing_submenu() {
     $current_url = basename(home_url($_SERVER['REQUEST_URI']));
-    Log::good_error_log($current_url);
     return $current_url == 'admin.php?page=wawp-licensing';
 }
 
 function license_submitted() {
     $current_url = basename(home_url($_SERVER['REQUEST_URI']));
-    Log::good_error_log($current_url);
     return $current_url == 'admin.php?page=wawp-licensing&settings-updated=true';
 }
 
