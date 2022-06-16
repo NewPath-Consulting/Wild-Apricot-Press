6208cbd6a5b874fb8f1fe4c24e309a30573c7fcb
Natalie Brotherton <brona@umich.edu>
2022-06-16 16:44:21 -0400

    add get current url function and more checking for settings pages

--- src/helpers.php
+++ src/helpers.php
@@ -6,13 +6,27 @@ const CORE_SLUG = 'wawp';
 const CORE_NAME = 'NewPath Wild Apricot Press (WAP)';
 
 function is_licensing_submenu() {
-    $current_url = basename(home_url($_SERVER['REQUEST_URI']));
+    $current_url = get_current_url();
     return $current_url == 'admin.php?page=wawp-licensing';
 }
 
+function is_wawp_settings() {
+    $current_url = get_current_url();
+    return str_contains($current_url, CORE_SLUG);
+}
+
 function license_submitted() {
-    $current_url = basename(home_url($_SERVER['REQUEST_URI']));
+    $current_url = get_current_url();
     return $current_url == 'admin.php?page=wawp-licensing&settings-updated=true';
+}
+
+function is_wa_login_menu() {
+    $current_url = get_current_url();
+    return $current_url == 'admin.php?page=wawp-login';
+}
+
+function get_current_url() {
+    return basename(home_url($_SERVER['REQUEST_URI']));
 }
 
 ?>
