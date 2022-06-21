dc7ac270a3121e24d94ee8c130ea22e2b0625912
Natalie Brotherton <brona@umich.edu>
2022-06-21 15:22:36 -0400

    add flag for showing credentials notice only when plugin is activated, add bulk notice

--- src/Activator.php
+++ src/Activator.php
@@ -8,6 +8,7 @@ require_once __DIR__ . '/WAIntegration.php';
 class Activator {
 
 	const CORE = 'wawp';
+	const SHOW_NOTICE_ACTIVATION = 'show_notice_activation';
 
 	private $slug;
 	private $filename;
@@ -46,14 +47,15 @@ class Activator {
 	 */
 	public static function activate() {
 		// Log back into Wild Apricot if credentials are entered and a valid license key is provided
-		$stored_wa_credentials = get_option('wawp_wal_name');
-		$wawp_licenses = get_option(WAIntegration::WAWP_LICENSES_KEY);
-		if (isset($stored_wa_credentials) && $stored_wa_credentials != '' && !empty($wawp_licenses) && array_key_exists('wawp', $wawp_licenses) && $wawp_licenses['wawp'] != '') {
+		if (WAIntegration::valid_wa_credentials() && Addon::instance()::has_valid_license(CORE_SLUG)) {
 			// Run credentials obtained hook, which will read in the credentials in WAIntegration.php
 			do_action('wawp_wal_credentials_obtained');
 			// Also create CRON event to refresh the membership levels/groups
 			require_once('MySettingsPage.php');
 			MySettingsPage::setup_cron_job();
+		} else {
+			update_option(self::SHOW_NOTICE_ACTIVATION, 1);
+			do_action('wawp_wal_set_login_private');
 		}
 	}
 
@@ -65,31 +67,37 @@ class Activator {
 		$license_exists = Addon::instance()::has_valid_license($this->slug);
 		if (!$license_exists) {
 			update_option($this->license_req_option_name, 'false');
-		} else {
-			delete_option($this->license_req_option_name);
-		}
+		} 
 	}
 
-	/**
-	 * Registers license hooks
-	 */
-	public function register_license_hooks() {
-		$opt = get_option($this->license_req_option_name);
-		if ($opt == 'false' || $opt == 'invalid') {
-			add_action('admin_init', array($this, 'force_deactivate'));
-		}
-	}
 
 	/**
 	 * Checks the status of the WA Authorization credentials and the license key. Displays appropriate admin notice messages if either one is invalid or missing. 
 	 */
 	public function admin_notices_creds_check() {
-		// only display these messages on wawp settings page
-		if (!is_wawp_settings()) return;
+		// only display these messages on wawp settings page or plugin page right after plugin is activated
+		$should_show_notice = get_option(self::SHOW_NOTICE_ACTIVATION);
+		if (!is_wawp_settings() && !($should_show_notice && is_plugin_page())) return;
 
-		if (!WAIntegration::valid_wa_credentials()) {
-			$this->empty_wa_message();
-		} else {
+		// if it's the plugin page, set show notice to false so it doesn't appear every time you see the plugins page
+		// also prevent the activation message from showing
+		if (is_plugin_page()) {
+			update_option(self::SHOW_NOTICE_ACTIVATION, 0);
+			unset($_GET['activate']);
+		}
+
+
+		$valid_wa_creds = WAIntegration::valid_wa_credentials();
+		$valid_license = Addon::instance()::has_valid_license(CORE_SLUG);
+
+		if (!$valid_wa_creds) {
+			if (!$valid_license) {
+				// if both creds are invalid show one message telling the user to enter both instead of two separate messages
+				self::empty_creds_message();
+			} else {
+				self::empty_wa_message();
+			}
+			return;
 		}
 
 		// print out licensing messages
@@ -97,20 +105,23 @@ class Activator {
 
 	}
 
-	// private function empty_creds_message() {
-	// 	echo "<div class='notice notice-warning'><p>";
-	// 	echo "Please enter your ";
-	// 	echo "<a href=" . admin_url('admin.php?page=wawp-login') . ">Wild Apricot credentials</a>";
-	// 	echo " and ";
-	// 	echo 
-	// }
+	private function empty_creds_message() {
+		echo "<div class='notice notice-warning'><p>";
+		echo "Please enter your ";
+		echo "<a href=" . admin_url('admin.php?page=wawp-login') . ">Wild Apricot credentials</a>";
+		echo " and ";
+		echo "<a href=" . admin_url('admin.php?page=wawp-licensing') . "> license key</a>";
+		echo " in order to use the <strong>" . CORE_NAME . "</strong> functionality.";
+		echo "</p></div>";
+	}
 
 
 	private function empty_wa_message() {
 
 		echo "<div class='notice notice-warning'><p>";
 		echo "Please enter your Wild Apricot credentials in ";
-		echo "<a href=" . admin_url('admin.php?page=wawp-login') . ">Wild Apricot Press > Authorization</a>.";
+		echo "<a href=" . admin_url('admin.php?page=wawp-login') . ">Wild Apricot Press > Authorization</a>";
+		echo " in order to use the <strong>" . CORE_NAME . "</strong> functionality.";
 		echo "</p></div>";
 	}
 
